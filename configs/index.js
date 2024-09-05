be722433912acea282af0907945349cfd4571dcc
Sharon <a0916503489@gmail.com>
2024-09-06 00:22:29 +0800

    門市查詢(老師檔案未改)

---
+++ configs/index.js
@@ -0,0 +1,6 @@
+export const PORT = 6005
+export const DEV = true
+
+// express 的位置
+export const apiBaseUrl = 'http://localhost:3005/api'
+export const avatarBaseUrl = 'http://localhost:3005/avatar'
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '@/styles/globals.css'
+
+export default function MyApp({ Component, pageProps }) {
+  // 使用自訂在頁面層級的版面(layout)
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
