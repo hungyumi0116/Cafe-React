ed5112010dc5be489fdbdbe2f81fbd288de197c6
Sharon <a0916503489@gmail.com>
2024-09-27 14:57:12 +0800

    09.27前端

--- pages/store/list.js
+++ pages/store/list.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import Navbar from '@/components/layout/default-layout/navbar'
 import Image from 'next/image'
-import ImageCarousel from './ImageCarousel'
 import store from '@/styles/store.module.css'
 import list from '@/styles/list.module.css'
 import Carousel from 'react-bootstrap/Carousel'
@@ -32,11 +31,25 @@ export default function List() {
           </h6>
         </div>
       </div>
-      <div className="body-pic">
-        <div className={[list.pic1, 'w-100'].join(' ')}>
-          <div className="d-flex align-items-center"></div>
+      
+        <div className={[list.body1, 'w-100'].join(' ')}>
+        <div className="d-flex justify-content-center">
+        <div className={[list.pic01].join(' ')}>
+          <img
+            src="/01.jpg"
+            style={{
+              width: 650,
+              height: 525,
+            }}
+          />
+        </div>
+        <div className={[list.vertical, 'd-flex', 'flex-column'].join(' ')}>
+          <img src="/pic2.jpg" className={list.pic2} />
+          <img src="/pic3.jpg" className={list.pic3} />
+          <img src="/pic4.jpg" className={list.pic4} />
         </div>
       </div>
+      
       <div className="">
         <div
           className="mx-auto w-100 d-flex pb-4"
@@ -141,137 +154,151 @@ export default function List() {
           </div>
         </div>
       </div>
-      <div className="body-pic">
-        <div className={[list.pic2, 'w-100'].join(' ')}>
-          <div className="d-flex align-items-center"></div>
+
+      <div className={[list.body2, 'w-100'].join(' ')}>
+        <div className="d-flex justify-content-center">
+          <div className={[list.pic01].join(' ')}>
+            <img
+              src="/01.jpg"
+              style={{
+                width: 650,
+                height: 525,
+              }}
+            />
+          </div>
+          <div className={[list.vertical, 'd-flex', 'flex-column'].join(' ')}>
+            <img src="/pic2.jpg" className={list.pic2} />
+            <img src="/pic3.jpg" className={list.pic3} />
+            <img src="/pic4.jpg" className={list.pic4} />
+          </div>
         </div>
       </div>
-      <div className="">
-        <div
-          className="mx-auto w-100 d-flex pb-4"
-          style={{
-            width: 1440,
-            backgroundColor: '#1C1B1B',
-          }}
-        >
-          <div className="d-flex mx-auto" style={{ maxWidth: 1440 }}>
-            <div>
-              <div className={[list.title].join(' ')}>
-                <h2
-                  style={{
-                    color: '#F37423',
-                  }}
-                >
-                  高雄岡山門市
-                </h2>
-                <div
-                  className={[
-                    list.roundedbox,
-                    'rounded-pill',
-                    'fw-normal',
-                  ].join(' ')}
-                >
-                  <div className={[list.citytext, 'fw-normal'].join(' ')}>
-                    高雄市/岡山區
-                  </div>
+
+      <div
+        className="mx-auto w-100 d-flex pb-4"
+        style={{
+          width: 1440,
+          backgroundColor: '#1C1B1B',
+        }}
+      >
+        <div className="d-flex mx-auto" style={{ maxWidth: 1440 }}>
+          <div>
+            <div className={[list.title].join(' ')}>
+              <h2
+                style={{
+                  color: '#F37423',
+                }}
+              >
+                高雄岡山門市
+              </h2>
+              <div
+                className={[list.roundedbox, 'rounded-pill', 'fw-normal'].join(
+                  ' '
+                )}
+              >
+                <div className={[list.citytext, 'fw-normal'].join(' ')}>
+                  高雄市/岡山區
                 </div>
               </div>
-
-              <div class="fw-normal">
-                <p className={[list.content, 'text-light'].join(' ')}>
-                  我們的咖啡廳注重質感與溫暖氛圍，精心挑選來自世界各地的高品質咖啡豆，
-                  由技藝精湛的咖啡師用心沖調每一杯咖啡。無論是追求純粹的風味還是獨特的
-                  口感，我們都致力於為您帶來最難忘的咖啡體驗。
-                </p>
-              </div>
-              <div class="pt-5">
-                <button
-                  className={[list.botton, 'rounded-pill', 'fw-normal'].join(
-                    ' '
-                  )}
-                >
-                  INSTAGRAM
-                </button>
-                <button
-                  className={[list.botton, 'rounded-pill', 'fw-normal'].join(
-                    ' '
-                  )}
-                >
-                  GOOGLE MAPS
-                </button>
-              </div>
             </div>
 
-            <div class="d-flex flex-column">
-              <div className={[list.btheight, 'd-flex'].join(' ')}>
-                <button
-                  className={[
-                    list.botton,
-                    'rounded-3',
-                    'fw-normal',
-                    'small',
-                  ].join(' ')}
-                >
-                  <FaWifi />
-                  提供wifi
-                </button>
-                <button
-                  className={[
-                    list.botton,
-                    'rounded-3',
-                    'fw-normal',
-                    'small',
-                  ].join(' ')}
-                >
-                  <ImPowerCord />
-                  提供插座
-                </button>
-                <button
-                  className={[
-                    list.botton,
-                    'rounded-3',
-                    'fw-normal',
-                    'small',
-                  ].join(' ')}
-                >
-                  <GiCoffeePot />
-                  手沖體驗門市
-                </button>
-                <button
-                  className={[
-                    list.botton,
-                    'rounded-3',
-                    'fw-normal',
-                    'small',
-                  ].join(' ')}
-                >
-                  <FaDog />
-                  寵物友善門市
-                </button>
-              </div>
-
-              <p className={[list.detailtext, 'text-light'].join(' ')}>
-                營業時間：10:00-18:00
-                <br />
-                公休日：固定每周三
-                <br />
-                TEL：07-5454456
+            <div class="fw-normal">
+              <p className={[list.content, 'text-light'].join(' ')}>
+                我們的咖啡廳注重質感與溫暖氛圍，精心挑選來自世界各地的高品質咖啡豆，
+                由技藝精湛的咖啡師用心沖調每一杯咖啡。無論是追求純粹的風味還是獨特的
+                口感，我們都致力於為您帶來最難忘的咖啡體驗。
               </p>
             </div>
-          </div>
-        </div>
-        <div className="body-pic">
-          <div className={[list.pic1, 'w-100'].join(' ')}>
-            <div
-              className={[list.swiper, 'd-flex', 'align-items-center'].join(
-                ' '
-              )}
-            >
-              <img src="/pic2.jpg" />
+            <div class="pt-5">
+              <button
+                className={[list.botton, 'rounded-pill', 'fw-normal'].join(' ')}
+              >
+                INSTAGRAM
+              </button>
+              <button
+                className={[list.botton, 'rounded-pill', 'fw-normal'].join(' ')}
+              >
+                GOOGLE MAPS
+              </button>
             </div>
+          </div>
+
+          <div class="d-flex flex-column">
+            <div className={[list.btheight, 'd-flex'].join(' ')}>
+              <button
+                className={[
+                  list.botton,
+                  'rounded-3',
+                  'fw-normal',
+                  'small',
+                ].join(' ')}
+              >
+                <FaWifi />
+                提供wifi
+              </button>
+              <button
+                className={[
+                  list.botton,
+                  'rounded-3',
+                  'fw-normal',
+                  'small',
+                ].join(' ')}
+              >
+                <ImPowerCord />
+                提供插座
+              </button>
+              <button
+                className={[
+                  list.botton,
+                  'rounded-3',
+                  'fw-normal',
+                  'small',
+                ].join(' ')}
+              >
+                <GiCoffeePot />
+                手沖體驗門市
+              </button>
+              <button
+                className={[
+                  list.botton,
+                  'rounded-3',
+                  'fw-normal',
+                  'small',
+                ].join(' ')}
+              >
+                <FaDog />
+                寵物友善門市
+              </button>
+            </div>
+
+            <p className={[list.detailtext, 'text-light'].join(' ')}>
+              營業時間：10:00-18:00
+              <br />
+              公休日：固定每周三
+              <br />
+              TEL：07-5454456
+            </p>
           </div>
         </div>
       </div>
+
+      <div className="d-flex justify-content-center">
+        <div className={[list.pic01].join(' ')}>
+          <img
+            src="/01.jpg"
+            style={{
+              width: 650,
+              height: 525,
+            }}
+          />
+        </div>
+        <div className={[list.vertical, 'd-flex', 'flex-column'].join(' ')}>
+          <img src="/pic2.jpg" className={list.pic2} />
+          <img src="/pic3.jpg" className={list.pic3} />
+          <img src="/pic4.jpg" className={list.pic4} />
+        </div>
+      </div>
+
       <div className="">
         <div
           className="mx-auto w-100 d-flex pb-4"
---
+++ pages/store/reseve_modal.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Resevemodal() {
+  return <div></div>
+}
