c0c1889928f2f25edfc28b9ecc6e021287d5863f
greensodasc <greensodasc@gmail.com>
2024-10-11 09:33:53 +0800

    10.11

--- pages/store/[store_id].js
+++ pages/store/[store_id].js
@@ -23,8 +23,8 @@ export default function Storeid() {
     {
       store_id: 1,
       store_pic1: `/01.jpg`,
-      store_pic2: `/10.jpg`,
-      store_pic3: `/05.jpg`,
+      store_pic2: `/02.jpg`,
+      store_pic3: `/18.jpg`,
       store_name: `信義ATT門市店`,
       store_description: `我們的咖啡廳注重質感與溫暖氛圍，精心挑選來自世界各地的高品質咖啡豆，由技藝精湛的咖啡師用心沖調每一杯咖啡。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
       store_city: `台北市`,
@@ -36,13 +36,12 @@ export default function Storeid() {
       store_time: `11:00-22:00`,
       store_close: `每周一`,
       store_phone: `02-77378707`,
-
     },
     {
       store_id: 2,
-      store_pic1: `/02.jpg`,
+      store_pic1: `/03.jpg`,
       store_pic2: `/04.jpg`,
-      store_pic3: `/05.jpg`,
+      store_pic3: `/15.jpg`,
       store_name: `JR東日本大飯店門市`,
       store_description: `嚴選來自世界各地的精品咖啡豆，由專業咖啡師精心製作，為您打造溫馨、雅緻的咖啡時光無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
       store_city: `台北市`,
@@ -57,9 +56,9 @@ export default function Storeid() {
     },
     {
       store_id: 3,
-      store_pic1: `/06.jpg`,
-      store_pic2: `/08.jpg`,
-      store_pic3: `/09.jpg`,
+      store_pic1: `/05.jpg`,
+      store_pic2: `/06.jpg`,
+      store_pic3: `/11.jpg`,
       store_name: `中山旗艦門市`,
       store_description: `以自然光充足的空間為主題，漫步咖啡坊提供多樣化的精品咖啡及自製甜點，是讀書、工作或與朋友小聚的最佳選擇，無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
       store_city: `台北市`,
@@ -74,9 +73,9 @@ export default function Storeid() {
     },
     {
       store_id: 4,
-      store_pic1: `/11.jpg`,
-      store_pic2: `/12.jpg`,
-      store_pic3: `/13.jpg`,
+      store_pic1: `/07.jpg`,
+      store_pic2: `/08.jpg`,
+      store_pic3: `/10.jpg`,
       store_name: `板橋門市`,
       store_description: `以溫馨的氛圍和柔和的燈光打造放鬆的空間，搭配香醇咖啡與手工甜點讓人留連忘返。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
       store_city: `新北市`,
@@ -89,10 +88,146 @@ export default function Storeid() {
       store_close: `每周二`,
       store_phone: `02-89696655`,
     },
+    {
+      store_id: 5,
+      store_pic1: `/09.jpg`,
+      store_pic2: `/10.jpg`,
+      store_pic3: `/13.jpg`,
+      store_name: `南勢角門市`,
+      store_description: `以輕鬆明亮的設計吸引顧客，專注於高品質咖啡與季節限定甜點，是愜意的日常休憩之選。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `新北市`,
+      area_city: `中和區`,
+      store_address: `捷運路63號`,
+      store_type: `1,3`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/tt2qyfcxe6xFMUmc8`,
+      store_time: `7:00-21:00`,
+      store_close: `每周一、二`,
+      store_phone: `02-29425922`,
+    },
+    {
+      store_id: 6,
+      store_pic1: `/11.jpg`,
+      store_pic2: `/12.jpg`,
+      store_pic3: `/13.jpg`,
+      store_name: `龍享門市`,
+      store_description: `以優質早餐和手工咖啡聞名，清晨咖啡屋提供活力滿滿的早晨時光，讓您以充沛精神展開一天。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `新竹縣`,
+      area_city: `竹北市`,
+      store_address: `自強南路36號1F`,
+      store_type: `1,3`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/xoUuruBaqibxFnF28`,
+      store_time: `9*:00-22:00`,
+      store_close: `每周一、二`,
+      store_phone: `03-6670663`,
+    },
+    {
+      store_id: 7,
+      store_pic1: `/13.jpg`,
+      store_pic2: `/14.jpg`,
+      store_pic3: `/18.jpg`,
+      store_name: `崇德門市`,
+      store_description: `以輕鬆明亮的設計吸引顧客，專注於高品質咖啡與季節限定甜點，是愜意的日常休憩之選。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `台中市`,
+      area_city: `北屯區`,
+      store_address: `崇德路三段207號`,
+      store_type: `1,3`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/uHcCHrpfRZ9bbrer9`,
+      store_time: `9:00-22:00`,
+      store_close: `每周二、三`,
+      store_phone: `04-24220302`,
+    },
+    {
+      store_id: 8,
+      store_pic1: `/15.jpg`,
+      store_pic2: `/26.jpg`,
+      store_pic3: `/10.jpg`,
+      store_name: `永華門市`,
+      store_description: `以輕鬆明亮的設計吸引顧客，專注於高品質咖啡與季節限定甜點，是愜意的日常休憩之選。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `台南市`,
+      area_city: `安平區`,
+      store_address: `永華路二段705號`,
+      store_type: `1,3`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/pr6YTKaKcXsmuxhj6`,
+      store_time: `9:00-21:30`,
+      store_close: `每周二、三`,
+      store_phone: `06-2956980`,
+    },
+    {
+      store_id: 9,
+      store_pic1: `/16.jpg`,
+      store_pic2: `/28.jpg`,
+      store_pic3: `/15.jpg`,
+      store_name: `台鋁門市`,
+      store_description: `以輕鬆明亮的設計吸引顧客，專注於高品質咖啡與季節限定甜點，是愜意的日常休憩之選。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `高雄市`,
+      area_city: `前鎮區`,
+      store_address: `忠勤路8號`,
+      store_type: `1,2,3`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/iFGAXJmJDjT433569`,
+      store_time: `10:00-21:30`,
+      store_close: `每周三`,
+      store_phone: `07-5369929`,
+    },
+    {
+      store_id: 10,
+      store_pic1: `/17.jpg`,
+      store_pic2: `/19.jpg`,
+      store_pic3: `/11.jpg`,
+      store_name: `巨蛋門市`,
+      store_description: `自家烘焙的咖啡豆散發出迷人的香氣，豆子咖啡工坊擁有專業咖啡師現場沖泡，適合咖啡愛好者細細品味。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `高雄市`,
+      area_city: `鼓山區`,
+      store_address: `文忠路2號`,
+      store_type: `1,2,3,4,5`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/iFGAXJmJDjT433569`,
+      store_time: `9:00-21:30`,
+      store_close: `依照門市公告`,
+      store_phone: `07-5525885`,
+    },
+    {
+      store_id: 11,
+      store_pic1: `/24.jpg`,
+      store_pic2: `/25.jpg`,
+      store_pic3: `/04.jpg`,
+      store_name: `文山門市`,
+      store_description: `午後時光以其溫暖的氛圍和溫馨的裝潢著稱，專注於手沖咖啡和健康輕食，提供讓人放鬆的休憩空間。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `高雄市`,
+      area_city: `鼓山區`,
+      store_address: `文衡路489號`,
+      store_type: `1,2,5`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/A9YVdrBnpX2Y4Ufv6`,
+      store_time: `9:00-21:30`,
+      store_close: `每周三`,
+      store_phone: `07-7678997`,
+    },
+    {
+      store_id: 12,
+      store_pic1: `/21.jpg`,
+      store_pic2: `/22.jpg`,
+      store_pic3: `/15.jpg`,
+      store_name: `高美門市`,
+      store_description: `隱身於巷弄中的小店，樹蔭咖啡提供舒適靜謐的環境，特調手沖咖啡與招牌輕食，讓您遠離塵囂靜心享受。無論是追求純粹的風味還是獨特的口感，我們都致力於為您帶來最難忘的咖啡體驗。`,
+      store_city: `高雄市`,
+      area_city: `鼓山區`,
+      store_address: `文美術東二路432號`,
+      store_type: `1,2,5`,
+      store_ig: `https://www.instagram.com/coffee/`,
+      store_goole: `https://maps.app.goo.gl/A9YVdrBnpX2Y4Ufv6`,
+      store_time: `9:00-21:30`,
+      store_close: `依照門市公告`,
+      store_phone: `07-5223238`,
+    },
   ])
-  const [StoreFilter, setStoreFilter] = useState([]);
-  const [StoreFilterName, setStoreFilterName] = useState("");
-  const [StoreFilterAddress, setStoreFilterAddress] = useState("");
+  const [StoreFilter, setStoreFilter] = useState([])
+  const [StoreFilterName, setStoreFilterName] = useState('')
+  const [StoreFilterAddress, setStoreFilterAddress] = useState('')
   // const [Reserve, setReserve] = useState([]) //第二張資料表
   const [total, setTotal] = useState(0) //總筆數
   const [pageCount, setPageCount] = useState(0) //總頁數
@@ -141,12 +276,12 @@ export default function Storeid() {
   const [isChecked4, setIsChecked4] = useState(false)
 
   const handleFilterNameChange = (e) => {
-    e.preventDefault();
-    setStoreFilterName(e.target.value);
+    e.preventDefault()
+    setStoreFilterName(e.target.value)
   }
   const handleFilterAddressChange = (e) => {
-    e.preventDefault();
-    setStoreFilterAddress(e.target.value);
+    e.preventDefault()
+    setStoreFilterAddress(e.target.value)
   }
   // 處理核取方塊狀態變化的函數
   const handleCheckboxChange = (event) => {
@@ -166,36 +301,47 @@ export default function Storeid() {
   }
 
   const handleFilterStore = (event) => {
-    console.log(StoreFilterName);
-    console.log(StoreFilterAddress);
-    console.log(isChecked);
-    console.log(isChecked1);
-    console.log(isChecked2);
-    console.log(isChecked3);
-    console.log(isChecked4);
-    let newFilterStore = Store.filter(s => s.store_name.indexOf(StoreFilterName) > -1)
-      .filter(s => s.store_address.indexOf(StoreFilterAddress) > -1);
+    console.log(StoreFilterName)
+    console.log(StoreFilterAddress)
+    console.log(isChecked)
+    console.log(isChecked1)
+    console.log(isChecked2)
+    console.log(isChecked3)
+    console.log(isChecked4)
+    let newFilterStore = Store.filter(
+      (s) => s.store_name.indexOf(StoreFilterName) > -1
+    ).filter((s) => s.store_address.indexOf(StoreFilterAddress) > -1)
     if (isChecked) {
       // 1:免費提供wifi
-      newFilterStore = newFilterStore.filter(s => s.store_type.split(',').indexOf("1") > -1);
+      newFilterStore = newFilterStore.filter(
+        (s) => s.store_type.split(',').indexOf('1') > -1
+      )
     }
     if (isChecked1) {
       // 2:手沖體驗門市
-      newFilterStore = newFilterStore.filter(s => s.store_type.split(',').indexOf("2") > -1);
+      newFilterStore = newFilterStore.filter(
+        (s) => s.store_type.split(',').indexOf('2') > -1
+      )
     }
     if (isChecked2) {
       // 3:免費提供插座
-      newFilterStore = newFilterStore.filter(s => s.store_type.split(',').indexOf("3") > -1);
+      newFilterStore = newFilterStore.filter(
+        (s) => s.store_type.split(',').indexOf('3') > -1
+      )
     }
     if (isChecked3) {
       // 4:寵物友善門市
-      newFilterStore = newFilterStore.filter(s => s.store_type.split(',').indexOf("4") > -1);
+      newFilterStore = newFilterStore.filter(
+        (s) => s.store_type.split(',').indexOf('4') > -1
+      )
     }
     if (isChecked4) {
       // 5:冰滴咖啡販售門市
-      newFilterStore = newFilterStore.filter(s => s.store_type.split(',').indexOf("5") > -1);
+      newFilterStore = newFilterStore.filter(
+        (s) => s.store_type.split(',').indexOf('5') > -1
+      )
     }
-    setStoreFilter(newFilterStore);
+    setStoreFilter(newFilterStore)
   }
 
   // ---這段是點擊右圖換左圖---
@@ -203,18 +349,18 @@ export default function Storeid() {
 
   // 初始點擊右圖換左圖之左圖圖片
   useEffect(() => {
-    Store.map(s => {
+    Store.map((s) => {
       if (leftImg[s.store_id] === undefined) {
         leftImg[s.store_id] = s.store_pic1
       }
-    });
-    setStoreFilter(Store);
-  }, Store);
+    })
+    setStoreFilter(Store)
+  }, Store)
 
   const handleClickRightImage = function (store_id, imgsrc) {
-    leftImg[store_id] = imgsrc;
-    const newObj = JSON.parse(JSON.stringify(leftImg));
-    setLeftImg(newObj);
+    leftImg[store_id] = imgsrc
+    const newObj = JSON.parse(JSON.stringify(leftImg))
+    setLeftImg(newObj)
   }
   // ---這段是點擊右圖換左圖(結尾)
 
@@ -368,7 +514,8 @@ export default function Storeid() {
                   冰滴咖啡販售門市
                 </label>
                 <div className="d-flex">
-                  <button className={store.selectbutton1}
+                  <button
+                    className={store.selectbutton1}
                     onClick={handleFilterStore}
                   >
                     查詢
@@ -401,22 +548,30 @@ export default function Storeid() {
                   />
                 </div>
                 <div
-                  className={[store.vertical, 'd-flex', 'flex-column'].join(' ')}
+                  className={[store.vertical, 'd-flex', 'flex-column'].join(
+                    ' '
+                  )}
                 >
                   <img
                     src={way.store_pic1}
                     className={store.pic2}
-                    onClick={() => handleClickRightImage(way.store_id, way.store_pic1)}
+                    onClick={() =>
+                      handleClickRightImage(way.store_id, way.store_pic1)
+                    }
                   />
                   <img
                     src={way.store_pic2}
                     className={store.pic3}
-                    onClick={() => handleClickRightImage(way.store_id, way.store_pic2)}
+                    onClick={() =>
+                      handleClickRightImage(way.store_id, way.store_pic2)
+                    }
                   />
                   <img
                     src={way.store_pic3}
                     className={store.pic4}
-                    onClick={() => handleClickRightImage(way.store_id, way.store_pic3)}
+                    onClick={() =>
+                      handleClickRightImage(way.store_id, way.store_pic3)
+                    }
                   />
                 </div>
               </div>
@@ -446,7 +601,9 @@ export default function Storeid() {
                           'fw-normal',
                         ].join(' ')}
                       >
-                        <div className={[store.citytext, 'fw-normal'].join(' ')}>
+                        <div
+                          className={[store.citytext, 'fw-normal'].join(' ')}
+                        >
                           {way.store_city}/{way.area_city}
                         </div>
                       </div>
@@ -488,18 +645,10 @@ export default function Storeid() {
           </div>
         ))}
 
-        {StoreFilter.length == 0 && (
-          <div>查無店家</div>
-        )}
-
-
+        {StoreFilter.length == 0 && <div>查無店家</div>}
       </div>
 
-      <ReserviceModal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-      />
-
+      <ReserviceModal isOpen={isModalOpen} onRequestClose={closeModal} />
     </>
   )
 }
